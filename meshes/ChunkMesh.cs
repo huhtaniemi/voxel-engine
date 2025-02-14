158dd072f987f030c68d81cddbd3c2df29ec3c23
Artur Huhtaniemi <artur.huhtaniemi@gmail.com>
2025-02-14 22:19:19 +0200

    refactored chunkmesh - removed redundant code GetVAO()

--- meshes/ChunkMesh.cs
+++ meshes/ChunkMesh.cs
@@ -7,9 +7,6 @@ public class ChunkMesh : BaseMesh
     private Chunk chunk;
     private int formatSize;
 
-    /// <summary>
-    /// Initializes the ChunkMesh class with a reference to the Chunk instance, sets the shader program, VBO format, format size, and attribute names, and creates the VAO.
-    /// </summary>
     public ChunkMesh(Chunk chunk) : base()
     {
         this.app = chunk.app;
@@ -27,9 +24,6 @@ public class ChunkMesh : BaseMesh
         this.vao = GetVAO();
     }
 
-    /// <summary>
-    /// Builds the chunk mesh and converts the vertex data to a float array.
-    /// </summary>
     protected override float[] GetVertexData()
     {
         uint[] mesh = VoxelMeshBuilder.BuildChunkMesh(
@@ -41,31 +35,4 @@ public class ChunkMesh : BaseMesh
         return mesh.Select(v => (float)v).ToArray();
     }
 
-    /// <summary>
-    /// Creates and initializes the VBO and VAO, sets up the vertex attributes, and returns the VAO.
-    /// </summary>
-    protected int GetVAO()
-    {
-        float[] vertexData = GetVertexData();
-        int vbo = GL.GenBuffer();
-        GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
-        GL.BufferData(BufferTarget.ArrayBuffer, vertexData.Length * sizeof(float), vertexData, BufferUsageHint.StaticDraw);
-
-        int vao = GL.GenVertexArray();
-        GL.BindVertexArray(vao);
-
-        int stride = CalculateStride(vboFormat);
-        int offset = 0;
-
-        for (int i = 0; i < attrs.Length; i++)
-        {
-            int location = GL.GetAttribLocation(program, attrs[i]);
-            GL.EnableVertexAttribArray(location);
-            GL.VertexAttribPointer(location, GetAttributeSize(vboFormat, i), VertexAttribPointerType.Float, false, stride, offset);
-            offset += GetAttributeSize(vboFormat, i) * sizeof(float);
-        }
-
-        GL.BindVertexArray(0);
-        return vao;
-    }
 }
