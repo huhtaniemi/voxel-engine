01bcee585c17afb8b1c42c34545602e97e41d7c7
Artur Huhtaniemi <artur.huhtaniemi@gmail.com>
2025-02-14 22:34:31 +0200

    fixed chunk class

--- world_objects/Chunk.cs
+++ world_objects/Chunk.cs
@@ -1,15 +1,20 @@
 using OpenTK.Mathematics;
 using System;
+using static Settings;
+
 
 public class Chunk
 {
     public VoxelEngine app;
+    /*
     public World world;
     public Vector3i Position { get; private set; }
     public Matrix4 MModel { get; private set; }
-    public byte[] voxels { get; set; }
+    */
+    public byte[] voxels { get; private set; }
 
-    public ChunkMesh mesh { get; private set; }
+    internal ChunkMesh? mesh { get; private set; }
+    /*
     public bool IsEmpty { get; private set; }
 
     public Vector3 Center { get; private set; }
@@ -30,7 +35,17 @@ public class Chunk
         this.Center = (position.ToVector3() + new Vector3(0.5f)) * Settings.CHUNK_SIZE;
         this.isOnFrustum = this.app.player.Frustum.IsOnFrustum;
     }
+    */
 
+    public Chunk(VoxelEngine app)
+    {
+        this.app = app;
+        this.voxels = BuildVoxels();
+
+        BuildMesh();
+    }
+
+    /*
     /// <summary>
     /// Sets the uniform for the chunk's model matrix.
     /// </summary>
@@ -43,32 +58,36 @@ public class Chunk
     {
         app.shader_program.SetUniform(mesh.program, "m_model", MModel);
     }
+    */
+
     /// <summary>
     /// Builds the mesh for the chunk.
     /// </summary>
-    public void BuildMesh()
-    {
-        mesh = new ChunkMesh(this);
-    }
+    private void BuildMesh() =>
+        this.mesh = new ChunkMesh(this);
 
     /// <summary>
     /// Renders the chunk if it is not empty and is within the frustum.
     /// </summary>
     public void Render()
     {
+        /*
         if (!IsEmpty && isOnFrustum(this))
         {
             SetUniform();
             mesh.Render();
         }
+        */
+        this.mesh?.Render();
     }
 
     /// <summary>
     ///  Builds the voxels for the chunk and checks if the chunk is empty.
     /// </summary>
-    public byte[] BuildVoxels()
+    static private byte[] BuildVoxels()
     {
-        var voxels = new byte[Settings.CHUNK_VOL];
+        var voxels = new byte[CHUNK_VOL];
+        /*
         var (cx, cy, cz) = Position * Settings.CHUNK_SIZE;
         GenerateTerrain(ref voxels, cx, cy, cz);
 
@@ -76,9 +95,24 @@ public class Chunk
         {
             IsEmpty = false;
         }
+        */
+        for (byte x = 0; x < CHUNK_SIZE; x++)
+        {
+            for (byte z = 0; z < CHUNK_SIZE; z++)
+            {
+                 for (byte y = 0; y < CHUNK_SIZE; y++)
+                {
+                    // todo: noise
+                    voxels[x + CHUNK_SIZE * z + CHUNK_AREA * y] =
+                        1;
+                }
+            }
+        }
         return voxels;
     }
 
+
+    /*
     /// <summary>
     ///  Generates the terrain for the chunk.
     /// </summary>
@@ -101,5 +135,6 @@ public class Chunk
             }
         }
     }
+    */
 }
 
