da4de8c39293b2a34c8f32d7f4234e02be4dee8a
Artur Huhtaniemi <artur.huhtaniemi@gmail.com>
2025-02-19 23:41:38 +0200

    refactored Textures class - cleanup - deleted generated bitmap and opengl code

--- Textures.cs
+++ Textures.cs
@@ -1,8 +1,6 @@
 using OpenTK.Graphics.OpenGL4;
 using System;
-using System.Drawing;
-//using System.Drawing.Common;
-using System.Drawing.Imaging;
+
 
 public class Textures
 {
@@ -11,10 +9,6 @@ public class Textures
     private int texture1;
     private int textureArray0;
 
-    /// <summary>
-    /// Takes an VoxelEngine instance and a method to load a texture from a file.
-    /// Initializes the Textures class, loads the textures, and assigns them to texture units.
-    /// </summary>
     public Textures(VoxelEngine app)
     {
         this.app = app;
@@ -30,55 +24,22 @@ public class Textures
         Use(texture1, 2);
     }
 
-    /// <summary>
-    /// Generates a texture ID, loads the image, uploads it to the GPU, and sets the texture parameters.
-    /// Loads a texture from a file. If isTexArray is true, it creates a texture array.
-    /// </summary>
     private int Load(string fileName, bool isTexArray = false)
     {
-#pragma warning disable CA1416
-        Bitmap bitmap = new($"assets/{fileName}");
-        bitmap.RotateFlip(RotateFlipType.RotateNoneFlipX);
-#pragma warning restore CA1416
+        //textureimg = pg.image.load(f'assets/{file_name}')
+        //textureimg = pg.transform.flip(textureimg, flip_x = True, flip_y = False)
 
         int textureId = GL.GenTexture();
-        GL.BindTexture(isTexArray ? TextureTarget.Texture2DArray : TextureTarget.Texture2D, textureId);
-
-#pragma warning disable CA1416
-        if (isTexArray)
-        {
-            int numLayers = 3 * bitmap.Height / bitmap.Width;
-            GL.TexImage3D(TextureTarget.Texture2DArray, 0, PixelInternalFormat.Rgba, bitmap.Width, bitmap.Height / numLayers, numLayers, 0,
-                OpenTK.Graphics.OpenGL4.PixelFormat.Bgra, PixelType.UnsignedByte, IntPtr.Zero);
-        }
-        else
-        {
-            BitmapData data = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height),
-            ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
-
-            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0,
-                OpenTK.Graphics.OpenGL4.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
-
-            bitmap.UnlockBits(data);
-        }
-#pragma warning restore CA1416
-
-        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Nearest);
-        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Nearest);
-        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
-        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
-
-        GL.BindTexture(TextureTarget.Texture2D, 0);
+        //texture = self.ctx.texture(
+        //    size = texture.get_size(),
+        //    components = 4,
+        //    data = pg.image.tostring(texture, 'RGBA', False)
+        //);
 
         return textureId;
     }
 
-    /// <summary>
-    /// Binds the texture to a specified texture unit.
-    /// </summary>
     private void Use(int textureId, int location)
     {
-        GL.ActiveTexture(TextureUnit.Texture0 + location);
-        GL.BindTexture(TextureTarget.Texture2D, textureId);
     }
 }
